0371f1d09facec128c5ac887c9c70d1701450b18
onpa <onpa@onpa-PC>
2017-02-28 14:22:38 +0900

    2단계 완전 개발 끝

--- security/src/main/java/com/security/controller/HomeController.java
+++ security/src/main/java/com/security/controller/HomeController.java
@@ -6,10 +6,14 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.security.domain.UserVO;
 
 /**
  * Handles requests for the application home page.
@@ -22,22 +26,32 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	@Secured("ROLE_USER")
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model,
+			@RequestParam(value = "user", defaultValue = "", required = true) String user) {
+		HomeService homeService = new HomeService();
+		
 		logger.info("Welcome home! The client locale is {}.", locale);
-
+		UserVO userVO = new UserVO();
+		userVO.setUser_name(user);
+		homeService.getUser(userVO);
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
 		String formattedDate = dateFormat.format(date);
-
 		model.addAttribute("serverTime", formattedDate);
-
 		return "home";
 	}
 
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/admin", method = RequestMethod.GET)
 	public String admin() {
+		return "admin";
+	}
+
+	@Secured("ROLE_USER")
+	@RequestMapping(value = "/admin/test", method = RequestMethod.GET)
+	public String admin_test() {
 		return "admin";
 	}
 
---
+++ security/src/main/java/com/security/controller/HomeService.java
@@ -0,0 +1,18 @@
+package com.security.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+import com.security.domain.UserVO;
+
+@Service
+public class HomeService {
+	private static final Logger logger = LoggerFactory.getLogger(HomeService.class);
+
+	@PreAuthorize("#userVO.user_name == authentication.name or hasRole(‘ROLE_ADMIN')")
+	public void getUser(UserVO userVO) {
+		logger.info("getUser success");
+	}
+}
