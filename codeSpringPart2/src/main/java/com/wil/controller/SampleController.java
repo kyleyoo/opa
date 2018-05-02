062c324d99de77f914545abfec03c4f3132b50be
onpa-PC\onpa <gogohs0116@naver.com>
2018-05-02 19:42:41 +0900

    코드로 배우는 스프링 웹프로젝트 Part3 P.346

---
+++ codeSpringPart2/src/main/java/com/wil/controller/SampleController.java
@@ -0,0 +1,11 @@
+package com.wil.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample")
+public class SampleController {
+	
+
+}
